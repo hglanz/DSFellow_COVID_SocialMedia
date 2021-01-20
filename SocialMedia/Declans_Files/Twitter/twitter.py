50fb6930df707cba5b969bc0162d8cd534b3c81c
Declan Molony <declanmolony@yahoo.com>
2021-01-20 11:03:17 -0800

    january covid

--- SocialMedia/Declans_Files/Twitter/twitter.py
+++ SocialMedia/Declans_Files/Twitter/twitter.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 
 
-#Query by Username
-#The code below will scrape for 100 tweets by a username then provide a CSV file with Pandas
+# Query by Username
+# The code below will scrape for 100 tweets by a username I choose, then it will make a CSV file with Pandas
 
 
 # Setting variables to be used below
@@ -28,7 +28,9 @@ tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text',
 #tweets_df1.head()
 
 # Export dataframe into a CSV
-tweets_df1.to_csv('decdec2323.csv', sep=',', index=False)
+#tweets_df1.to_csv('decdec2323.csv', sep=',', index=False)
+
+##################################################################################################
 
 maxTweets = 500
 
@@ -48,4 +50,22 @@ tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text',
 
 
 # Export dataframe into a CSV
-tweets_df2.to_csv('elephants.csv', sep=',', index=False)
+#tweets_df2.to_csv('elephants.csv', sep=',', index=False)
+
+##################################################################################################
+
+maxTweets = 500
+
+# Creating list to append tweet data to
+tweets_list3 = []
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('covid since:2020-01-01 until:2020-01-07').get_items()):
+    if i>maxTweets:
+        break
+    tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+
+tweets_df3 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
+# Export dataframe into a CSV
+tweets_df3.to_csv('january_covid.csv', sep=',', index=False)
